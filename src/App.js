47251756a6435b13329b321532259269f08a837b
JingWei-S <weijing.524@gmail.com>
2023-04-09 11:47:30 +1000

    create basic project skeleton

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+
+function App() {
+  const [score, setScore] = useState(0);
+  const [best, setBest] = useState(0);
+  const [gameState, setGameState] = useState(true);
+
+  const incrementScore = () => {
+    setScore(score + 1);
+  };
+
+  useEffect(() => {
+    const changeGameState = () => {
+      if (score > 5) {
+        setGameState(false);
+      }
+    };
+
+    document.addEventListener("click", changeGameState);
+  });
+
+  useEffect(() => {
+    const updateBest = () => {
+      if (gameState === false) {
+        setBest(score)
+        setGameState(true)
+      }
+    };
+    
+    updateBest();
+  })
+
+  return (
+    <div className="App">
+      <p>Current Score: {score}</p>
+      <button onClick={incrementScore}>Click me to add score</button>
+      <p>Best Score: {best}</p>
+      <p>Game Status: {String(gameState)}</p>
+    </div>
+  );
+}
+
+export default App;
