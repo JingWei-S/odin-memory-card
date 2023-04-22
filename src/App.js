92de2b54e61a8f191ec0725ec776a439dd98db1c
JingWei-S <weijing.524@gmail.com>
2023-04-22 16:17:17 +1000

    complete the project

--- src/App.js
+++ src/App.js
@@ -6,6 +6,29 @@ function App() {
   const [score, setScore] = useState(0);
   const [best, setBest] = useState(0);
   const [gameState, setGameState] = useState(true);
+  const [images, setImages] = useState([
+    { src: "/img/img1.png", clicked: false },
+    { src: "/img/img2.png", clicked: false },
+    { src: "/img/img3.png", clicked: false },
+    { src: "/img/img4.png", clicked: false },
+    { src: "/img/img5.png", clicked: false },
+    { src: "/img/img6.png", clicked: false },
+    { src: "/img/img7.png", clicked: false },
+    { src: "/img/img8.png", clicked: false },
+    { src: "/img/img9.png", clicked: false },
+    { src: "/img/img10.png", clicked: false },
+    { src: "/img/img11.png", clicked: false },
+    { src: "/img/img12.png", clicked: false },
+  ]);
+
+  const shuffleArray = (array) => {
+    const newArray = array.slice();
+    for (let i = newArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
+    }
+    return newArray;
+  };
 
   const incrementScore = () => {
     setScore(score + 1);
@@ -13,6 +36,25 @@ function App() {
 
   const stopGame = () => {
     setGameState(false);
+  };
+
+  const restartGame = () => {
+    setGameState(true);
+    // also need to reset the image state
+    setImages(shuffleArray([
+      { src: "/img/img1.png", clicked: false },
+      { src: "/img/img2.png", clicked: false },
+      { src: "/img/img3.png", clicked: false },
+      { src: "/img/img4.png", clicked: false },
+      { src: "/img/img5.png", clicked: false },
+      { src: "/img/img6.png", clicked: false },
+      { src: "/img/img7.png", clicked: false },
+      { src: "/img/img8.png", clicked: false },
+      { src: "/img/img9.png", clicked: false },
+      { src: "/img/img10.png", clicked: false },
+      { src: "/img/img11.png", clicked: false },
+      { src: "/img/img12.png", clicked: false },
+    ]));
   };
 
   useEffect(() => {
@@ -23,33 +65,26 @@ function App() {
       setScore(0);
     }
   });
-  // useEffect(() => {
-  //   const changeGameState = () => {
-  //     if (score > 5) {
-  //       setGameState(false);
-  //     }
-  //   };
-  //   document.addEventListener("click", changeGameState);
-  // });
-
-  // here is how I implement the game on logic
-  // useEffect(() => {
-  //   const updateBest = () => {
-  //     if (gameState === false) {
-  //       setBest(score)
-  //       setGameState(true)
-  //     }
-  //   };
-
-  //   updateBest();
-  // });
 
   return (
     <div className="App">
-      <p>Current Score: {score}</p>
-      <p>Best Score: {best}</p>
-      <p>Game Status: {String(gameState)}</p>
-      <CardDisplay incrementScore={incrementScore} stopGame={stopGame} />
+      <div className="score">
+        <p>Current Score: {score}</p>
+        <p>Best Score: {best}</p>
+        {!gameState ? <button onClick={restartGame}>Restart</button> : <></>}
+      </div>
+      <div className="instruction">Get points for choosing a card, but don't click on the same card more than once 👻</div>
+      <CardDisplay
+        incrementScore={incrementScore}
+        stopGame={stopGame}
+        gameState={gameState}
+        images={images}
+        setImages={setImages}
+        shuffleArray={shuffleArray}
+      />
+      <div className="footer">
+        <p>Team Work by Jing and Dall-E 2 🍪</p>
+      </div>
     </div>
   );
 }
--- src/Components/CardDisplay.js
+++ src/Components/CardDisplay.js
@@ -1,45 +1,42 @@
 import React, { useEffect, useState } from "react";
 
 function CardDisplay(props) {
-  const [images, setImages] = useState([
-    { src: "/img/img1.png", clicked: false },
-    { src: "/img/img2.png", clicked: false },
-    { src: "/img/img3.png", clicked: false },
-    { src: "/img/img4.png", clicked: false },
-    { src: "/img/img5.png", clicked: false },
-    { src: "/img/img6.png", clicked: false },
-    { src: "/img/img7.png", clicked: false },
-    { src: "/img/img8.png", clicked: false },
-    { src: "/img/img9.png", clicked: false },
-    { src: "/img/img10.png", clicked: false },
-    { src: "/img/img11.png", clicked: false },
-    { src: "/img/img12.png", clicked: false },
-  ]);
-
+  // const [images, setImages] = useState([
+  //   { src: "/img/img1.png", clicked: false },
+  //   { src: "/img/img2.png", clicked: false },
+  //   { src: "/img/img3.png", clicked: false },
+  //   { src: "/img/img4.png", clicked: false },
+  //   { src: "/img/img5.png", clicked: false },
+  //   { src: "/img/img6.png", clicked: false },
+  //   { src: "/img/img7.png", clicked: false },
+  //   { src: "/img/img8.png", clicked: false },
+  //   { src: "/img/img9.png", clicked: false },
+  //   { src: "/img/img10.png", clicked: false },
+  //   { src: "/img/img11.png", clicked: false },
+  //   { src: "/img/img12.png", clicked: false },
+  // ]);
+  const images = props.images;
+  const setImages = props.setImages;
   // shuffle the array
-  const shuffleArray = (array) => {
-    const newArray = array.slice();
-    for (let i = newArray.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [newArray[i], newArray[j]] = [newArray[j], newArray[i]];
-    }
-    return newArray;
-  };
+  const shuffleArray = props.shuffleArray;
 
   //   shuffle every time
   const handleShuffleImages = (e) => {
-    const curImageSrc = e.target.src.replace(/^.*\/\/[^/]+/, "");
-    // implement the logic to check if the image has been clicked or not
-    const newArray = [...images];
-    const imageIndex = images.findIndex((image) => image.src === curImageSrc);
-    if (newArray[imageIndex].clicked) {
+    const gameState = props.gameState;
+    if (gameState) {
+      const curImageSrc = e.target.src.replace(/^.*\/\/[^/]+/, "");
+      // implement the logic to check if the image has been clicked or not
+      const newArray = [...images];
+      const imageIndex = images.findIndex((image) => image.src === curImageSrc);
+      if (newArray[imageIndex].clicked) {
         props.stopGame();
-    } else {
+      } else {
         // if not clicked
         // add points
-      newArray[imageIndex].clicked = true;
-      setImages(shuffleArray(newArray));
-      props.incrementScore();
+        newArray[imageIndex].clicked = true;
+        setImages(shuffleArray(newArray));
+        props.incrementScore();
+      }
     }
   };
 
@@ -49,7 +46,6 @@ function CardDisplay(props) {
 
   return (
     <div className="card-display">
-      {/* <button onClick={handleShuffleImages}>随机排序</button> */}
       {images.map((image, index) => (
         <img
           key={`image ${index + 1}`}
